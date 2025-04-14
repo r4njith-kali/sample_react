50aa444b1c03c90de3e5d01a2ad92d831dfc8dbe
r4njith-kali <e0958148@u.nus.edu>
2025-04-14 19:28:13 +0800

    Checking out h1-h6 and p

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
   return (
     <div className = "App">
       <h1>Hi Dev!</h1>
+      <h2>Hi Dev!</h2>
+      <h3>Hi Dev!</h3>
+      <h4>Hi Dev!</h4>
+      <h5>Hi Dev!</h5>
+      <h6>Hi Dev!</h6>
+      <p>Hi Dev!</p>
     </div>
   )
 }
