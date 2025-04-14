01526772cbd15b08e1bcd6066cc5561ac02f21e9
r4njith-kali <e0958148@u.nus.edu>
2025-04-14 19:45:59 +0800

    Testing out html tags

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,22 @@ import './App.css'
 function App() {
   return (
     <div className = "App">
-      <h1>Hi Dev!</h1>
-      <h2>Hi Dev!</h2>
-      <h3>Hi Dev!</h3>
-      <h4>Hi Dev!</h4>
-      <h5>Hi Dev!</h5>
-      <h6>Hi Dev!</h6>
-      <p>Hi Dev!</p>
+      <header>
+        <h1>Hi Dev!</h1>
+      </header>
+      <main> 
+        <h2>Hello! Select an option</h2> 
+        <select>
+          <option>A</option>
+          <option>B</option>
+          <option>C</option>
+          <option>D</option>
+          <input type = 'text' placeholder='Enter a text' />
+        </select>
+      </main>
+      <footer>
+        by ranjith
+      </footer>
     </div>
   )
 }
