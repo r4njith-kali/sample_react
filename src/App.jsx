a1fcbc9ad03ae8a36e9506f9c7e161fec786ff90
r4njith-kali <e0958148@u.nus.edu>
2025-04-14 20:02:55 +0800

    Added button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  const nodeLabel = "Idea";
+//  const nodeLabel = "Idea";
   const nodeStyle = {
     border: '2px solid blue',
     padding: '10px 20px',
@@ -10,12 +11,18 @@ function App() {
     backgroundColor: 'lightblue'
   };
 
+  const [nodeLabel, setNodeLabel] = useState('Idea');
+
   return (
     <div className = "App">
       <h1>My MindMap</h1>
       <div style = {nodeStyle}>
         {nodeLabel}
       </div>
+      <br />
+      <button onClick={() => setNodeLabel("Updated Idea")}>
+        Rename
+      </button>
       <p>Let's start building {2+2}</p>
     </div>
   );
