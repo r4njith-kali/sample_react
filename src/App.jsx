633dddea47f8a18397472bd7c48ae1fa00eff96d
r4njith-kali <e0958148@u.nus.edu>
2025-04-15 10:38:41 +0800

    Button feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import './App.css'
 import MindMapNode from './MindMapNode2'
 
 function App() {
+    const [nodeLabel, setNodeLabel] = useState('True');
+
     return (
         <div>
             <h1>Hi</h1>
+            <button onClick= {() => setNodeLabel("Updated Idea")}>
+                Rename
+            </button>
         </div>
     )
 }
+
+export default App;
 
 
