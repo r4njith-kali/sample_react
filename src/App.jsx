1fee1b6178577c49472c73389cbc3ad1e8f4e246
r4njith-kali <e0958148@u.nus.edu>
2025-04-15 10:35:08 +0800

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,12 @@ import React, { useState } from 'react';
 import './App.css'
 import MindMapNode from './MindMapNode2'
 
+function App() {
+    return (
+        <div>
+            <h1>Hi</h1>
+        </div>
+    )
+}
+
 
